6f7591dbb7ef8b628013f105c1ba4cef86d02037
MITHUN M <142463879+mithun-03@users.noreply.github.com>
2024-03-20 12:22:09 +0530

    Update app.py  with

--- Average_Calculator/app.py
+++ Average_Calculator/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']='Mithun@2003'
-app.config["MYSQL_DB"]="AVG_CAL"
+app.config["MYSQL_DB"]="AVG_CAL"                   # created tables for prime,rand,even,fibo in avg_cal tables
 app.config["MYSQL_CURSORCLASS"]="DictCursor"
 
 mysql = MySQL(app)
@@ -17,15 +17,16 @@ mysql = MySQL(app)
 
 class Average:
 
-  def primes(self):
-    nums = requests.get("http://20.244.56.144/numbers/primes").json()
+  def operation(self,type,table_name):
+    nums = requests.get(f"http://20.244.56.144/numbers/{type}").json()
+    test_server_res=nums["numbers"]
     numbers = set(nums['numbers'])
     numbers = list(numbers)
     numbers.sort()
 
 
     cur = mysql.connection.cursor()
-    cur.execute('select num from prime ')
+    cur.execute(f'select num from {table_name} ')
     data = cur.fetchall()
     prev_data =set()
 
@@ -42,16 +43,18 @@ class Average:
       LENGTH = len(new_data)
       new_data = new_data[LENGTH-10: LENGTH]
     
-    cur.execute('delete from prime')
+    cur.execute(f'delete from {table_name}')
     cur.connection.commit()
 
     for num in new_data:
       print(num)
-      cur.execute('insert into prime(num) values(%s)',(num,))
+      cur.execute(f'insert into {table_name}(num) values(%s)',(num,))
     cur.connection.commit()
     cur.close()
     prev_data =list(prev_data)
-    return [prev_data,new_data]
+    return [prev_data,new_data,test_server_res]
+  
+  
       
 
       
@@ -64,10 +67,19 @@ class Average:
 def numbers(id):
   obj = Average()
   if id=='p':
-    nums=obj.primes()
-    res ={'prev_window':nums[0], 'curr_window':nums[1],'avg':sum(nums[1])/len(nums[1])}
- 
-    return jsonify(res)
+    nums=obj.operation("primes","prime")
+  elif id=='r': 
+    nums=obj.operation("rand","rand")
+  elif id=='e':
+    nums=obj.operation("even","even")
+  else:
+    nums=obj.operation("fibo","fibo")
+
+  res ={'numbers':nums[2],'window_prev_state':nums[0], 'window_curr_state':nums[1],'avg':sum(nums[1])//len(nums[1])}
+  return jsonify(res)
+  
+  
+
 
 
 if __name__ =='__main__':
